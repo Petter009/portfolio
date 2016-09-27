f94fb4bccc4502b98b3dc1345544da28b50a1a71
petter009 <pc>
2016-09-27 10:38:08 +0200

    done

--- 09_Portfolio/PortFolioTests.cs
+++ 09_Portfolio/PortFolioTests.cs
@@ -10,34 +10,34 @@ namespace _09_Portfolio
 
     //THESE ARE THE THIRD TESTS YOU SHOULD DO 
 
-//    [TestClass]
-//    public class PortFolioTests
-//    {
-//        Stock stockHP = new Stock("HPQ", 60.03, 120);
-//        Stock stockIBM = new Stock("IBM", 32.11, 50);
-//        SavingsAccount cd1000 = new SavingsAccount("Account 556677", 1000, 4.5);
+    [TestClass]
+    public class PortFolioTests
+    {
+        Stock stockHP = new Stock("HPQ", 60.03, 120);
+        Stock stockIBM = new Stock("IBM", 32.11, 50);
+        SavingsAccount cd1000 = new SavingsAccount("Account 556677", 1000, 4.5);
 
 
-//        [TestMethod]
-//        public void PortfolioCanUseConstructor()
-//        {
-//            List<Asset> stocks = new List<Asset>();
-//            stocks.Add(stockHP);
-//            stocks.Add(stockIBM);
-//            Portfolio portfolio = new Portfolio(stocks);
-//            Assert.AreEqual(8809.1, portfolio.GetTotalValue());
-//        }
+        [TestMethod]
+        public void PortfolioCanUseConstructor()
+        {
+            List<IAsset> stocks = new List<IAsset>();
+            stocks.Add(stockHP);
+            stocks.Add(stockIBM);
+            Portfolio portfolio = new Portfolio(stocks);
+            Assert.AreEqual(8809.1, portfolio.GetTotalValue());
+        }
 
-//        [TestMethod]
-//        public void CanAddAssets()
-//        {
-//            Portfolio portfolio = new Portfolio();
-//            portfolio.AddAsset(stockHP);
-//            portfolio.AddAsset(stockIBM);
-//            portfolio.AddAsset(cd1000);
-//            Assert.AreEqual(9809.1, portfolio.GetTotalValue());
-//        }
+        [TestMethod]
+        public void CanAddAssets()
+        {
+            Portfolio portfolio = new Portfolio();
+            portfolio.AddAsset(stockHP);
+            portfolio.AddAsset(stockIBM);
+            portfolio.AddAsset(cd1000);
+            Assert.AreEqual(9809.1, portfolio.GetTotalValue());
+        }
 
        
-//    }
+    }
 }
---
+++ 09_Portfolio/Portfolio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09_Portfolio
+{
+    internal class Portfolio
+    {
+        private List<IAsset> stocks;
+
+        public Portfolio(List<IAsset> stocks)
+        {
+            this.stocks = stocks;
+        }
+
+        public Portfolio()
+        {
+            stocks = new List<IAsset>();
+        }
+
+        internal double GetTotalValue()
+        {
+            double result = 0;
+            foreach (var Asset in stocks)
+            {
+                result += Asset.GetValue();
+            }
+            return result; 
+        }
+
+        internal void AddAsset(IAsset asset)
+        {
+            stocks.Add(asset);
+        }
+
+    }
+}
